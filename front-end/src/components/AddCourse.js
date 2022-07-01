b08763053403ab4ef9e6c5b6e5d61f4ce4bcac40
Sean <craig.sean92@gmail.com>
2022-07-01 15:20:31 -0600

    created readme

--- front-end/src/components/AddCourse.js
+++ front-end/src/components/AddCourse.js
@@ -1,14 +1,18 @@
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 
-const AddCourse = ({courses}) => {
-
+const AddCourse = ({professors}) => {
+ 
   const [name, setName] = useState(null)
+  const [profId, setProfId] = useState(null)
   const navigate = useNavigate();
 
   
   function handleChange(e){
-      setName(e.target.value)
+      setName(e.target.userInput)
+  }
+  function handleSelectChange(e){
+      setProfId(e.target.professors)
   }
 
   function handleSubmit(e){
@@ -18,7 +22,7 @@ const AddCourse = ({courses}) => {
           'Accept': 'application/json',
           'Content-Type': 'application/json'
           }
-      const body = {subject: name}
+      const body = {subject: name, professor_id: profId }
       const options = {
           method: "POST",
           headers,
@@ -35,8 +39,14 @@ const AddCourse = ({courses}) => {
     <div>
       <form onSubmit={handleSubmit}>
         <h1>New Course</h1>
-        <label>Course Name: </label>
-        <input type="text" name="user-input"onChange={ handleChange } autoFocus={true}/>
+          <label>Course Name: </label>
+            <input type="text" name="userInput"onChange={ handleChange } autoFocus={true}/>
+          <label for="professors">Select Professor Name: </label>
+            <select name="professors" id="professors" onChange={handleSelectChange}>
+              {professors.map((prof) => {
+                return <option value={prof.id}>{prof.name}</option>
+              })}
+            </select>
         <br />
         <input type='submit' value='Submit' />
       </form>
--- front-end/src/components/CourseList.js
+++ front-end/src/components/CourseList.js
@@ -4,6 +4,7 @@ import AddCourse from './AddCourse'
 
 const CourseList = () => {
   const [courses, setCourses] = useState([])
+  const [professors, setProfessors] = useState([])
   const [newCourseForm, setNewCourseForm] = useState(false)
   
 
@@ -12,8 +13,13 @@ const CourseList = () => {
     fetch('http://localhost:9292/courses')
     .then((resp) => resp.json())
     .then((data) => setCourses(data))
+  },[])
 
-    console.log(courses.professors)
+  useEffect(() => {
+    fetch('http://localhost:9292/professors')
+    .then((resp) => resp.json())
+    .then((data) => setProfessors(data))
+
   },[])
 
   function showNewCourseForm(){
@@ -30,7 +36,7 @@ const CourseList = () => {
     <div>
       <h1>Courses</h1>
       <button onClick={showNewCourseForm}>Add New Course</button>
-        { newCourseForm ? <AddCourse courses={courses}/> : null }
+        { newCourseForm ? <AddCourse courses={courses} professors={professors}/> : null }
       <ul>
         { courseList }
       </ul>
